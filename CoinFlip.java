530bba9fca529b29ae157de8b1c7f39f2a9e134e
danielm012 <danielm012@live.com>
2016-11-23 18:26:24 -0500

    Update CoinFlip.java

--- CoinFlip.java
+++ CoinFlip.java
@@ -1,1 +1,28 @@
-Public 
+// Group Daniel, Sam, Randy, Victor
+
+/* Working 
+ Victor - Gui
+ Daniel - Gui support
+ Sam - 
+ Randy ?
+*/
+
+
+
+
+
+
+Public class CoinFlip {
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
