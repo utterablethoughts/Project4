245b957b06bcd7215caefb7700814f4aef19290d
utterablethoughts <samuel.aloysius.mitchell@gmail.com>
2016-11-23 19:24:21 -0500

    Update CoinFlip.java

--- CoinFlip.java
+++ CoinFlip.java
@@ -1,17 +1,3 @@
 import java.util.Random;
-Public class CoinFlip {
-
-public static void main(String[]args) {
-		//Sam- this is our randomized coin flip
-		 String[] HeadsTails = {"Heads","Tails"};
-		 Random random = new Random();
-		 int index = random.nextInt(HeadsTails.length);
-		 //Sam- with every "RUN" it will generate a random
-		 //between Heads and Tails only
-		 System.out.println(HeadsTails[index]);
-		
-	}
-
-
 
 }
